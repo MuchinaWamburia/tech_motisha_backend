75a49701555cdfc17593997fb5cf42dcebba9cbc
MuchinaWamburia <lucymuchina20@gmail.com>
2023-04-13 00:54:11 +0300

    tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  resources :video_comments
+  resources :blog_comments
+  resources :audio_comments
+  resources :videos
+  resources :audios
+  resources :blogs
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
